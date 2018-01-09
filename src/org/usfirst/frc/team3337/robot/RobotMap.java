8a4eb3c7bab597bf1cc217825af1d71a4b1637c4
Josuelmet <josuec2000@gmail.com>
2018-01-08 18:23:09 -0600

    Adding stuff to RobotMap.

--- src/org/usfirst/frc/team3337/robot/RobotMap.java
+++ src/org/usfirst/frc/team3337/robot/RobotMap.java
@@ -1,6 +1,13 @@
+//We're in this package.
 package org.usfirst.frc.team3337.robot;
 
+//This class is RobotMap.
 public class RobotMap
 {
+	/*
+	 * These numbers are all public (accessible to all), final (unchangeable),
+	 * and static (accessible without a RobotMap instance.
+	 */
+	
 	public final static int myInt = 0;
 }
