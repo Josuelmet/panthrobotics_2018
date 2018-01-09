535cacadfa3f012dafc4f9242c88c6d3296ed33e
GoldRush4983 <mpad825@gmail.com>
2018-01-08 18:43:18 -0600

    Created Drive

---
+++ src/main/src/org/usfirst/frc/team3337/robot/drive/Drive.java
@@ -0,0 +1,8 @@
+package main.src.org.usfirst.frc.team3337.robot.drive;
+
+import org.usfirst.frc.team3337.robot.RobotMap;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public abstract class Drive {
+
+}
--- src/main/src/org/usfirst/frc/team3337/robot/drive/TeleopDrive.java
+++ src/main/src/org/usfirst/frc/team3337/robot/drive/TeleopDrive.java
@@ -1,9 +1,11 @@
 package main.src.org.usfirst.frc.team3337.robot.drive;
 
+//FIRST Library Packages
 import org.usfirst.frc.team3337.controls.Controls;
 import org.usfirst.frc.team3337.controls.POVButton;
 import org.usfirst.frc.team3337.controls.ToggleButton;
 import org.usfirst.frc.team3337.robot.RobotMap;
+//WPI Library packages
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
