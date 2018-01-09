fd90c0261f1229ea0b9f9bbb15f7bdaa1797c966
GoldRush4983 <mpad825@gmail.com>
2018-01-08 18:34:15 -0600

    Troubleshootin, TeleopDrive, Initializing variables in Robot

---
+++ src/main/src/org/usfirst/frc/team3337/robot/drive/TeleopDrive.java
@@ -0,0 +1,14 @@
+package main.src.org.usfirst.frc.team3337.robot.drive;
+
+import org.usfirst.frc.team3337.controls.Controls;
+import org.usfirst.frc.team3337.controls.POVButton;
+import org.usfirst.frc.team3337.controls.ToggleButton;
+import org.usfirst.frc.team3337.robot.RobotMap;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class TeleopDrive {
+
+}
