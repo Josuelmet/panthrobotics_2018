4209f97c3981e0299492ef4dc2fa625affdcd577
GoldRush4983 <mpad825@gmail.com>
2018-01-10 17:57:07 -0600

    Changed stuff

--- src/main/src/org/usfirst/frc/team3337/drive/TeleopDrive.java
+++ src/main/src/org/usfirst/frc/team3337/drive/TeleopDrive.java
@@ -1,9 +1,6 @@
 package main.src.org.usfirst.frc.team3337.drive;
 
 //FIRST Library Packages
-import org.usfirst.frc.team3337.controls.Controls;
-import org.usfirst.frc.team3337.controls.POVButton;
-import org.usfirst.frc.team3337.controls.ToggleButton;
 import org.usfirst.frc.team3337.robot.RobotMap;
 //WPI Library packages
 import edu.wpi.first.wpilibj.Encoder;
@@ -13,6 +10,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public abstract class TeleopDrive
 {
-
+	Joystick stick1;
+	
+	
+	void init()
+	{
+		
+	}
 	
 }
