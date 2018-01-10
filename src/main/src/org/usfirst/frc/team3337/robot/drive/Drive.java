ba70fcf55f0f8dc73d2cbb650341f4a7ebe957ae
GoldRush4983 <mpad825@gmail.com>
2018-01-09 20:57:13 -0600

    Worked on Drive a little

--- src/main/src/org/usfirst/frc/team3337/robot/drive/Drive.java
+++ src/main/src/org/usfirst/frc/team3337/robot/drive/Drive.java
@@ -2,12 +2,13 @@ package main.src.org.usfirst.frc.team3337.robot.drive;
 
 import org.usfirst.frc.team3337.robot.RobotMap;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public abstract class Drive {
 
 	//Initializing Variables
 	double speedL, speedR;
 	
-	public Drive();
+	public Drive(TalonSRX leftFront, TalonSRX leftMiddle, TalonSRX leftBack. TalonSRX rightFront, TalonSRX rightMiddle, TalonSRX rightBack, double robotLimit);
 	
 }
