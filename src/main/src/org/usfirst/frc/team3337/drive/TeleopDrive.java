a5a839122c7d9b5824d278a71854889b1a567a09
Josuelmet <josuec2000@gmail.com>
2018-01-10 17:50:28 -0600

    Deleting stuff

--- src/main/src/org/usfirst/frc/team3337/drive/TeleopDrive.java
+++ src/main/src/org/usfirst/frc/team3337/drive/TeleopDrive.java
@@ -1,9 +1,6 @@
 package main.src.org.usfirst.frc.team3337.drive;
 
 //FIRST Library Packages
-import org.usfirst.frc.team3337.controls.Controls;
-import org.usfirst.frc.team3337.controls.POVButton;
-import org.usfirst.frc.team3337.controls.ToggleButton;
 import org.usfirst.frc.team3337.robot.RobotMap;
 //WPI Library packages
 import edu.wpi.first.wpilibj.Encoder;
