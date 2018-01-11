9b51b3b842c01645fdb9b1380ed77cbc16aecd2a
GoldRush4983 <mpad825@gmail.com>
2018-01-10 18:39:21 -0600

    made ports for triggers

--- src/main/src/org/usfirst/frc/team3337/drive/TeleopGameDrive.java
+++ src/main/src/org/usfirst/frc/team3337/drive/TeleopGameDrive.java
@@ -6,15 +6,16 @@ import edu.wpi.first.wpilibj.Joystick;
 public class TeleopGameDrive extends TeleopDrive
 {
 	
-	public TeleopGameDrive(TalonSRX _leftFront, TalonSRX _leftBack, TalonSRX _rightFront, TalonSRX _rightBack,
-			TalonSRX _swerveWheel, Joystick _driveStick) {
+	public TeleopGameDrive(TalonSRX _leftFront, TalonSRX _leftBack, TalonSRX _rightFront, TalonSRX _rightBack, TalonSRX _swerveWheel, Joystick _driveStick)
+	{
 		super(_leftFront, _leftBack, _rightFront, _rightBack, _swerveWheel, _driveStick);
-		// TODO Auto-generated constructor stub
 	}
-
-	double accelerationTrigger, brakeTrigger;
 	
-	accelerationTrigger = stick1.getRawAxis(3);
-	brakeTrigger = stick1.getRawAxis(2);
+	void triggers()
+	{
+		forwardTrigger = stick1.getRawAxis(3);
+		reverseTrigger = stick1.getRawAxis(2);
+	}
+		
 	
 }
